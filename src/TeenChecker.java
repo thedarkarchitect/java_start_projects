381958209a33722c0b7e39631d3f56b3366c8a40
thedarkarchitect <romanreynolds240@gmail.com>
2023-01-25 21:00:29 +0300

    fourth commit

--- src/TeenChecker.java
+++ src/TeenChecker.java
@@ -6,7 +6,7 @@ public class TeenChecker {
     }
 
     public static boolean TeenNumberChecker(int num1, int num2, int num3){
-        if ((num1 > 13 || num1 < 19) || (num2 > 13 || num2 < 19) || (num3 > 13 || num3 < 19)){
+        if ((num1 >= 13 && num1 <= 19) || (num2 >= 13 && num2 <= 19) || (num3 >= 13 && num3 <= 19)){
             return true;
         }else{
             return false;
