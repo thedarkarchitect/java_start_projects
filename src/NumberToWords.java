2023af2384fbb88cab6a1e578b7c23fdf7111619
thedarkarchitect <romanreynolds240@gmail.com>
2023-03-09 13:56:32 +0300

    ninteenth commit

--- src/NumberToWords.java
+++ src/NumberToWords.java
@@ -1,9 +1,26 @@
 public class NumberToWords {
     public static void main(String[] args){
-
+        getDigitCount(0);
     }
 
     public static void numberToWords(int num){
-        
+
+    }
+
+    public static void getDigitCount(int num){
+        if(num < 0){
+            System.out.println(-1);
+        }else{
+            int counter = 0;
+            while(num > 0 ){
+                int lastDigit = num % 10;
+                if (lastDigit >= 1 || lastDigit == 0){
+                    counter ++;
+                }
+                num /= 10;
+            }
+            
+            System.out.println(counter);
+        }
     }
 }
